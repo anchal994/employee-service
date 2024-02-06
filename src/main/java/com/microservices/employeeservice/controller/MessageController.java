5c9772a2359d8406cafa8f2f3ae4fc5b4c9fd57e
anchal994 <159000309+anchal994@users.noreply.github.com>
2024-02-06 16:20:43 +0530

    Update MessageController.java

--- src/main/java/com/microservices/employeeservice/controller/MessageController.java
+++ src/main/java/com/microservices/employeeservice/controller/MessageController.java
@@ -1,19 +1,19 @@
-package com.microservices.employeeservice.controller;
+// package com.microservices.employeeservice.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Value;
+// //import org.springframework.cloud.context.config.annotation.RefreshScope;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope
-@RestController
-public class MessageController {
+// //@RefreshScope
+// @RestController
+// public class MessageController {
 
-    @Value("${spring.boot.message}")
-    private String message;
+//     @Value("${spring.boot.message}")
+//     private String message;
 
-    @GetMapping("/users/message")
-    public String message(){
-        return message;
-    }
-}
+//     @GetMapping("/users/message")
+//     public String message(){
+//         return message;
+//     }
+// }
